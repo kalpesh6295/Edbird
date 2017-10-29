a2e3bb79646339aad040b3f0b2e4b366efbe363a
“kalpesh6295” <kalpesh6295@gmail.com>
2017-10-29 10:20:43 +0530

    changes in index

--- src/app/body/index/index.service.ts
+++ src/app/body/index/index.service.ts
@@ -4,18 +4,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class IndexService {
     public categories:IndexModel[]=[
-        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../assets/images/tattoo-artist.jpg'),
+        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../assets/images/tattoo_artist.jpg'),
         new IndexModel('Photographer','art never comes from happiness.','../../../assets/images/photographer.jpg'),
-        new IndexModel('Hairstyler','art never comes from happiness.','../../../assets/images/hairsalon.png'),
-        new IndexModel('Fashion Designer','art never comes from happiness.','../../../assets/images/fashion-design.jpg'),
+        new IndexModel('Hairstyler','art never comes from happiness.','../../../assets/images/hairsalon.jpg'),
+        new IndexModel('Fashion Designer','art never comes from happiness.','../../../assets/images/fashion_design.jpg'),
         new IndexModel('Animator','art never comes from happiness.','../../../../assets/images/animator.jpg'),
-        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo-artist.jpg'),
-        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo-artist.jpg'),
-        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo-artist.jpg'),
-        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo-artist.jpg'),
-        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo-artist.jpg'),
-        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo-artist.jpg'),
-        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo-artist.jpg')
+        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo_artist.jpg'),
+        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo_artist.jpg'),
+        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo_artist.jpg'),
+        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo_artist.jpg'),
+        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo_artist.jpg'),
+        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo_artist.jpg'),
+        new IndexModel('Tattoo-artist','art never comes from happiness.','../../../../assets/images/tattoo_artist.jpg')
 
     ]
 }
