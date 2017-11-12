bbb2ff5f7847df9c8eaa4a692fc00b8a5a26aaa0
“kalpesh6295” <kalpesh6295@gmail.com>
2017-11-12 14:30:36 +0530

    sign in fb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,26 +51,28 @@ export class AppComponent implements OnInit {
   //facebook sign in 
   
   provider = new firebase.auth.FacebookAuthProvider();
+  
   facebook_login(){
-   
-   firebase.auth().signInWithRedirect(this.provider);
-   firebase.auth().signInWithPopup(this.provider).then(function (result) {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-}).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-});
-
+    firebase.auth().signInWithRedirect(this.provider);
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+this.display=false;
   }
 
 }
